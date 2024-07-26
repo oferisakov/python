3c493f808fd7a5b29f1198b844cc18e6128eefbe
oferisakov <oferisak@gmail.com>
2024-07-26 11:26:07 +0300

    add lesson_12

---
+++ lesson_12/01_if_basics.py
@@ -0,0 +1,7 @@
+age = int(input("what is your age"))
+
+if age > 18:
+    print(age)
+    print("well done")
+else:
+    print('too small')
---
+++ lesson_12/02_if_elif.py
@@ -0,0 +1,9 @@
+age = int(input("what is your age"))
+
+if age > 18:
+    print(age)
+    print("well done")
+elif age == 18:
+    print("your age is 18")
+else:
+    print('too small')
